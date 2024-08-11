90261e69194ed9cc7c4603ee700ec5c68f26fb05
tye-dev <tianye0020@gmail.com>
2024-08-11 12:51:36 +0800

    feat: init rollup

--- packages/test.js
+++ packages/test.js
@@ -1,4 +1,11 @@
-const a = 1;
-let b = 2;
-
-console.log(a, b);
+// 实现链表结构
+function ListNode(val, next) {
+	this.val = val;
+	this.next = next;
+}
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+node1.next = node2;
+node2.next = node3;
+console.log(node1);
